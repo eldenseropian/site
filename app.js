7e329c7f1b424b95cd7f7ced24129b003aaec0c7
lilyseropian <lilyseropian@gmail.com>
2014-05-29 13:57:21 -0400

    Initial version of header and connect complete

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var app = module.exports = express();
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.json());
+	app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({src: __dirname + '/public'}));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+});
+
+app.configure('production', function() {
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+  res.render('index.jade', {title: 'Lily Seropian: Home'});
+});
+
+app.post('/', function(req, res) {
+	console.log('posted');
+	res.redirect('/pdfs/Seropian Resume.pdf');
+});
+
+var listening_app = app.listen(3000);
+
+console.log('Express server listening on port %d in %s mode',
+    listening_app.address().port, app.settings.env);
