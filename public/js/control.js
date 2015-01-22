9b2899c74a5294fbf0ed06f71f8331a87225f9e1
Lily Seropian <lilyseropian@gmail.com>
2015-01-22 17:19:23 -0500

    Rough draft of code card

--- public/js/control.js
+++ public/js/control.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-    card('#github', 'http://www.github.com/lilyseropian', 'github.com/lilyseropian', '../public/images/github.jpg', 'Connect');
-    card('#linkedin', 'http://www.linkedin.com/in/lilyseropian', 'linkedin.com/in/lilyseropian', '../public/images/linkedin.jpg', 'Connect');
-    card('#email', 'mailto:lilyseropian@gmail.com', 'lilyseropian@gmail.com', '../public/images/email.jpg', 'Connect');
-    card('#resume', '../public/pdfs/Seropian Resume.pdf', 'Resume', '../public/images/resume.jpg', 'Connect');
-    card('#cv', '../public/pdfs/Seropian CV.pdf', 'CV', '../public/images/cv.jpg', 'Connect');
+    codeCard('#shirk', 'Shirk', 'Group Project', 'October 2014',
+      '../public/images/shirk.jpg', 'Learn More', 'This academic project was implemented by Andre Aboulian, Tricia Divita, and Lily Seropian for 6.170 Software Studio.',
+      'http://shirk-aandre.rhcloud.com', 'https://github.com/shirk-team/shirk');
+
+    linkCard('#github', 'http://www.github.com/lilyseropian', 'github.com/lilyseropian', '../public/images/github.jpg', 'Connect');
+    linkCard('#linkedin', 'http://www.linkedin.com/in/lilyseropian', 'linkedin.com/in/lilyseropian', '../public/images/linkedin.jpg', 'Connect');
+    linkCard('#email', 'mailto:lilyseropian@gmail.com', 'lilyseropian@gmail.com', '../public/images/email.jpg', 'Connect');
+    linkCard('#resume', '../public/pdfs/Seropian Resume.pdf', 'Resume', '../public/images/resume.jpg', 'Connect');
+    linkCard('#cv', '../public/pdfs/Seropian CV.pdf', 'CV', '../public/images/cv.jpg', 'Connect');
 
     $('.tabular.menu .item').tab();
     $('.cards .image').dimmer({
@@ -12,7 +16,7 @@ $(document).ready(function() {
 
 });
 
-function card(selector, link, linkDisplay, imagePath, buttonText) {
+function linkCard(selector, link, linkDisplay, imagePath, buttonText) {
     $(selector).html(
       '<div class="dimmable image">' +
         '<div class="ui dimmer">' +
@@ -29,3 +33,26 @@ function card(selector, link, linkDisplay, imagePath, buttonText) {
       '</div>'
     );
 };
+
+function codeCard(selector, projectTitle, projectType, projectDate, imagePath, buttonText, projectDescription, projectSiteLink, projectCodeLink) {
+    $(selector).html(
+      '<div class="dimmable image">' +
+        '<div class="ui dimmer">' +
+          '<div class="content">' +
+            '<div class="center">' +
+              '<a class="ui inverted button" href="' + projectCodeLink + '">' + buttonText + '</a>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+        '<img src="' + imagePath + '" alt=":("/>' +
+      '</div>' +
+      '<div class="content">' +
+        '<a class="header" href="' + projectSiteLink + '">' + projectTitle + '</a>' +
+        '<div class="meta">' +
+          '<span>' + projectType + '</span>' +
+        '</div>' +
+        '<div class="description">' + projectDescription + '</div>' +
+      '</div>' + 
+      '<div class="extra content">' + projectDate + '</div>'
+    );
+}
